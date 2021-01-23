9082222fc989ff71757a98aa190c3e876dca2e25
GulTion <gkv8192@gmail.com>
2021-01-23 21:03:47 +0530

    a

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ export default class App extends Component {
     super();
     this.state={}
   }
-  componentDidMount(){
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
 
   render() {
     return (
